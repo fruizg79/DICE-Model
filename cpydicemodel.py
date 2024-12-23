e0b8c722802202d288bf6a0f942acf7b2e5acb2e
Fernando Ruiz <fruizg79@gmail.com>
2024-12-23 10:23:09 +0100

    Operative version - it works, but the output validation is still pending

--- cpydicemodel.py
+++ cpydicemodel.py
@@ -24,16 +24,19 @@ class cdicemodel:
         self.end_year = end_year
         self.industrial_emissions_initial_value = industrial_emissions_initial_value
         self.land_emissions_initial_value =land_emissions_initial_value
-        self.emission_intensity_initial_value = emission_intenisity_initial_value
+        self.emission_intenisity_initial_value = emission_intenisity_initial_value
         self.land_emissions_d  = 0.01
         self.total_emissions_initial_value   =  industrial_emissions_initial_value \
                                                 + land_emissions_initial_value
         self.carbon_concentration_initial_value = np.array([830.4,1527,10010 ])
-        self.carbon_emissions  =  np.array([self.total_emissions,0,0])
+        self.carbon_emissions  =  np.array([self.total_emissions_initial_value,0,0])
         self.T_AT = 0.8
         self.T_LO = 0.0068
         self.temperature_vector_initial_value = self.get_temperature_vector_initial_value()
-        self.emission_intensity_d = 0
+        self.temperature_vector_df        = self.get_temperature_vector_df(initial_year,
+                                                                                      self.T_AT,
+                                                                                      self.T_LO)
+        self.emission_intensity_d = 0.01
         # Abatement Cost:
         self.abatement_rate_initial_value       = abatement_rate_initial_value
         self.abatement_rate_d                   = 0.01
@@ -64,15 +67,23 @@ class cdicemodel:
     def get_df_output(self):
         df_output = pd.DataFrame(columns=["Y","K","I","L","A","U","T_AT",\
                                           "mu", "total_emissions","industrial_emissions",\
-                                              "land_emissions"],
+                                              "land_emissions","Q"],
                                  index = range(self.initial_year,self.end_year+1))
         return df_output
         
     def get_emission_intensity(self,year):
         tau = year - self.initial_year
-        emission_intensity = self.emission_intensity_initial*(1+self.emission_intensity_d)**-tau
+        emission_intensity = self.emission_intenisity_initial_value*(1+self.emission_intensity_d)**-tau
         return emission_intensity
-        
+    def get_temperature_vector_df(self,year,T_AT,T_LO):
+        if year == self.initial_year:
+            col_names=["T_AT","T_LO"]
+            df_temperature_vector = pd.DataFrame(columns=col_names,
+                            index = range(self.initial_year,self.end_year+1))
+        df_temperature_vector.loc[year]['T_AT'] = T_AT
+        df_temperature_vector.loc[year]['T_LO'] = T_LO
+        self.df_temperature_vector = df_temperature_vector
+        return df_temperature_vector
     def get_temperature_vector_initial_value(self):
         temperature_vector_initial_value = np.array([self.T_AT,self.T_LO])
         return temperature_vector_initial_value
@@ -90,28 +101,43 @@ class cdicemodel:
         L_g  = self.L_g*(1+self.L_d)**-tau
         A    = self.A0*(1 + A_g)**tau
         L    = self.L0*(1 +L_g)**tau
-        K    = self.get_capital(self,year)
+        K    = self.get_capital(year)
         Y  = A*K**self.alpha*L**(1-self.alpha)
         self.df_output.loc[year]["Y"] = Y
         self.df_output.loc[year]["L"] = L
         self.df_output.loc[year]["K"] = K
+        self.df_output.loc[year]["A"] = A
         return Y
-    
     def get_net_production(self,year,abatement_cost,damage_cost,Y):
-        Q = (1-abatement_cost)/(1-damage_cost)*Y
-        self.df_output[year]["Q"] = Q
+        Q = (1 - abatement_cost)/(1 + damage_cost)*Y
+        self.df_output.loc[year]["Q"] = Q
         return Q
-    
+    def get_capital(self,year):
+        if year==self.initial_year:
+            K = self.K0
+        else:
+            I = self.get_investment(year)
+            K = self.df_output.loc[year-1]['K']*(1-self.K_d) + I
+        self.df_output.loc[year]['K']=K
+        return K
+    def get_investment(self,year):
+        if year==self.initial_year:
+            I = 0
+        else:
+            Q = self.df_output.loc[year-1]["Q"]
+            I = self.s*Q # check this
+        self.df_output.loc[year]["I"] = I
+        return I
     def get_abatement_rate(self,year):
         if year==self.initial_year:
-            abatemenr_rate = 0            
+            abatemenr_rate = self.abatement_rate_initial_value            
         else:
             tau = year - self.initial_year-1
             abatemenr_rate = self.abatement_rate_initial_value*(1+self.abatement_rate_d)**-tau
         self.df_output.loc[year]["mu"] = abatemenr_rate
         return abatemenr_rate
 
-    def get_damage_cost(temperature_at):
+    def get_damage_cost(self,temperature_at):
         pi_1 = 0
         pi_2 = 2.67*10**-3
         omega = pi_1*temperature_at+pi_2*temperature_at**2
@@ -126,7 +152,7 @@ class cdicemodel:
     
     def get_land_emissions(self,year):
         tau = year - self.initial_year
-        land_emissions = self.land_emissions_initial_value*(1+self.and_emissions_d)**-tau
+        land_emissions = self.land_emissions_initial_value*(1+self.land_emissions_d)**-tau
         self.df_output.loc[year]["land_emissions"] = land_emissions
         return land_emissions
     
@@ -145,21 +171,26 @@ class cdicemodel:
         return total_emissions
     
     def get_discount_factor_utility(self,year):
-        discount_factor_utility = (1 +self.discount_rate_utility)**-(year \
-                                - self.initial_year)
+        tau = (year - self.initial_year)
+        discount_factor_utility = (1 +self.discount_rate_utility)**-tau
+        #discount_factor_utility = 0 # REVIEW THIS
         return discount_factor_utility
     
     def get_discounted_utility(self,total_consumption,
                                population,
                                current_year):
-            consumption_per_cap     = total_consumption/population
-            discount_factor_utility = (1 + self.discount_rate_utility)**-(current_year-self.initial_year)
-            discounted_utility      = self.get_utility_function(consumption_per_cap,self.theta)*population*discount_factor_utility
-            self.df_output[current_year]["U"] = discounted_utility
-            return discounted_utility
+        consumption_per_cap     = total_consumption/population
+        discount_factor_utility =  self.get_discount_factor_utility(current_year) 
+        if current_year > self.initial_year:
+            Utility_previous_value  = self.df_output.loc[current_year-1]['U']
+        else:
+            Utility_previous_value = 0
+        discounted_utility      =  Utility_previous_value \
+                                       +self.get_utility_function(consumption_per_cap) *population*discount_factor_utility
+        self.df_output.loc[current_year]["U"] = discounted_utility
+        return discounted_utility
         
-    def get_carbon_concentration(self,
-                                 carbon_concentration,
+    def get_carbon_concentration(self,year,
                                  carbon_emissions):
         """ CC = (CC_AT, CC_UP, CC_LO)
         
@@ -169,18 +200,23 @@ class cdicemodel:
             
             b_matrix = [phi_b1,0,0]
         """
-        phi_matrix           = np.array([[0.912, 0.0383, 0],
-                                     [0.088, 0.9592, 0.0003],
-                                     [0,0.0025,0.9997]])
-    
-        b_matrix             = np.array([0.2727,0,0])
-        
-
-        new_carbon_concentration = np.dot(phi_matrix,carbon_concentration) + np.dot(b_matrix,carbon_emissions)
-        
+        if year == self.initial_year:
+            new_carbon_concentration = self.carbon_concentration_initial_value
+            col_names = ['CC_AT', 'CC_UP', 'CC_LO']
+            self.df_carbon_concentration = pd.DataFrame(columns=col_names,
+                                                        index = range(self.initial_year,self.end_year+1))            
+        else:
+            phi_matrix           = np.array([[0.912, 0.0383, 0],
+                                         [0.088, 0.9592, 0.0003],
+                                         [0,0.0025,0.9997]])
+            b_matrix             = np.array([0.2727,0,0])
+            prior_carbon_concentration =  self.df_carbon_concentration.loc[year-1]
+            new_carbon_concentration = np.dot(phi_matrix,prior_carbon_concentration) \
+                                        + np.dot(b_matrix,carbon_emissions)
+        self.df_carbon_concentration.loc[year] = new_carbon_concentration
         return new_carbon_concentration
         
-    def get_exogenous_forcing(year):
+    def get_exogenous_forcing(self,year):
         base_year = 2010
         end_year  = 2100
         f_ex_base_year = 0.25 #w/m2
@@ -195,7 +231,7 @@ class cdicemodel:
         return F_ex
     
         
-    def get_radiative_forcing(cc_at,F_ex):
+    def get_radiative_forcing(self,cc_at,F_ex):
         # 
         # This function links accumulated carbon emissions in the atmosphere and global
         # warming at the Earth’s surface through increases in radiative forcing. 
@@ -217,7 +253,9 @@ class cdicemodel:
         radiative_forcing = nu/np.log(2)*np.log(cc_at/cc_at1750) + F_ex
         return radiative_forcing
         
-    def get_temperature_vector(temperature_vector,radiative_forcing):
+    def get_temperature_vector(self,
+                               year,
+                               radiative_forcing):
         """This system of equations means that “higher radiative forcing warms the atmospheric
          layer, which then warms the upper ocean, gradually warming the deep ocean
          
@@ -238,72 +276,67 @@ class cdicemodel:
         #  ep_hat_12 = ep_1*ep_3
         #  ep_hat21  = ep_4
         #  ep_hat22  = 1 - ep_4
-        
-        ep_1     =  0.098
-        ep_hat_11 = 86.30*10**-2
-        ep_hat_12 = 0.8624*10**-2
-        ep_hat_21 = 2.5*10**-2
-        ep_hat_22 = 97.5*10**-2
-        
-        P  = np.array([[ep_hat_11,ep_hat_12],
-                     [ep_hat_21,ep_hat_22]])
-        
-        B  = np.array([ep_1,0]) 
-        # T_AT_new, T_LO_new
-        temperature_vector_new = np.dot(P,temperature_vector)+np.dot(B,radiative_forcing)
-        return temperature_vector_new 
-    def get_capital(self,year):
-        if year==self.ininitial_year:
-            K = self.K0
-        else:
-            
-            K = self.df_output.loc[year-1]['K']*(1-self.K_d) \
-                + self.df_output.loc[year-1]['I']*(1-self.K_d)
-        self.df_output.loc[year]['K']=K
-        return K
-    def get_investment(self,year):
         if year==self.initial_year:
-            I = 0
-            self.out
+            T_AT = self.temperature_vector_initial_value[0]
+            T_LO = self.temperature_vector_initial_value[1]
+            temperature_vector_new = self.temperature_vector_initial_value
         else:
-            I = self.s*self.df_output.loc[year-1]["Q"] # check this
-        self.df_output.loc[year]["I"] = I
-        return I
+            ep_1     =  0.098
+            ep_hat_11 = 86.30*10**-2
+            ep_hat_12 = 0.8624*10**-2
+            ep_hat_21 = 2.5*10**-2
+            ep_hat_22 = 97.5*10**-2
+            P  = np.array([[ep_hat_11,ep_hat_12],
+                         [ep_hat_21,ep_hat_22]])
+            B  = np.array([ep_1,0])
+            temperature_vector_prior = self.temperature_vector_df.loc[year-1]
+            temperature_vector_new = np.dot(P,temperature_vector_prior)+np.dot(B,radiative_forcing)
+            
+        T_AT = temperature_vector_new[0]
+        T_LO = temperature_vector_new[1]
+        self.temperature_vector_df.loc[year] = temperature_vector_new
+        # self.get_temperature_vector_df(year,T_AT,T_LO)
+        self.df_output.loc[year]['T_AT'] = T_AT
+        self.df_output.loc[year]['T_LO'] = T_LO 
+        return temperature_vector_new 
+    
+
 
     def get_one_step(self,year):
-        try:
             Y                    = self.get_gross_production(year)
             abatement_rate       = self.get_abatement_rate(year)
             total_emissions      =  self.get_total_emissions(year, Y)
             carbon_emissions     =  np.array([total_emissions,0,0])
-            
-            
             F_ex                   = self.get_exogenous_forcing(year)
-            if year == self.initial_year:
-                carbon_concentration   = self.carbon_concentration_initial_value #
-                temperature_vector     = self.temperature_vector_initial_value
-            else:
-                carbon_concentration = self.get_carbon_concentration(carbon_concentration,carbon_emissions)
-                F_rad                  = self.get_radiative_forcing(carbon_concentration[0],F_ex)
-                temperature_vector     = self.get_temperature_vector(temperature_vector,F_rad)
+            # if year == self.initial_year:
+            #     carbon_concentration   = self.get_carbon_concentration(year,[]) #
+            #     temperature_vector     = self.get_temperature_vector(year,[])
+            # else:
+            #     carbon_concentration     = self.get_carbon_concentration(year,carbon_emissions)
+            #     F_rad                    = self.get_radiative_forcing(carbon_concentration[0],F_ex)
+            #     temperature_vector       = self.get_temperature_vector(year, F_rad)
+            
+            carbon_concentration     = self.get_carbon_concentration(year,carbon_emissions)
+            F_rad                    = self.get_radiative_forcing(carbon_concentration[0],F_ex)
+            temperature_vector       = self.get_temperature_vector(year, F_rad)
             
             temperature_at         = temperature_vector[0]
-            abatement_cost =  self.get_abatement_cost(abatement_rate)
-            damage_cost    =  self.get_damage_cost(temperature_at)
+            abatement_cost         =  self.get_abatement_cost(abatement_rate)
+            damage_cost            =  self.get_damage_cost(temperature_at)
             
             Q = self.get_net_production(year, abatement_cost, damage_cost, Y)
-            I = self.get_investment(year)
+           
             total_consumption = (1-self.s)*Q
-            population        = self.df_output[year]["L"]
+            population        = self.df_output.loc[year]["L"]
+            self.df_output.loc[year]["T_AT"] = temperature_at
             
             U = self.get_discounted_utility(total_consumption,population,year)
             output = "OK"
-        except:
-                output = "something wrong happened"
 
-        return output
+            output = "??"
+            return output
         
                 
 
-if __name__ == "__main__":
-    cdicemodel()
+#if __name__ == "__main__":
+#    cdicemodel()
--- script_example.py
+++ script_example.py
@@ -9,59 +9,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 """ Initial Values"""
-
+runfile('C:/Users/fruiz/Documents/GitHub/pydicemodel/cpydicemodel.py',
+        wdir='C:/Users/fruiz/Documents/GitHub/pydicemodel')
 
 initial_year = 2017
 end_year     = 2100
-
-# Climate variables - JUST FYI
-industrial_emissions               = 33.61 #gtCO2
-land_emissions_initial_value       = 3.3
-land_emissions_d                   = 0.01
-total_emissions                    =  industrial_emissions + land_emissions_initial_value
-
-carbon_concentration_initial_value = np.array([830.4,1527,10010 ])
-carbon_emissions                   = np.array([36.91,0,0])
-T_AT = 0.8
-T_LO = 0.0068
-temperature_vector_initial_value = np.array([T_AT,T_LO])
-emission_intensity = industrial_emissions/124.41655945341294
-emission_intensity_d = 0
-# Abatement Cost:
-abatement_rate_initial_value       = 0.25
-abatement_rate_d                   = 0.01
-emission_intensity_d               = 0.0
-theta_1 = 0.01 # TBC
-theta_2 = 2.6
+industrial_emissions_initial_value  = 33.61 #gtCO2
+land_emissions_initial_value        = 3.3
+emission_intenisity_initial_value   = 0.29
+population_initial_value            = 7.8
+capital_initial_value               = 318.773
+tfp_initial_value                   = 5.276
+abatement_rate_initial_value        = 0.99
 
 
 
-
-
-# economic variables
-s                 = 0.15 # Saving rate
-
-
-theta =  0.5
-rho   =  0.04
-
-# Production 
-alpha = 0.3
-A0   = 5.276
-A_g = 0.0075
-A_d = 0.0012
-
-
-L0   = 7.725
-L_g = 0.01
-L_d = 0.015
-
-K0 = 318.773
-K_d = 0.0625
-
-# Welfare function paramenters
-theta = 1.45
-discount_rate_utility = 0.015 
 
 
 dicemodel1 = cdicemodel(initial_year,
@@ -74,4 +36,7 @@ dicemodel1 = cdicemodel(initial_year,
                  tfp_initial_value,
                  abatement_rate_initial_value)
 
+for y in range(2017,2101):
+    dicemodel1.get_one_step(y)
 
+df = dicemodel1.df_output
